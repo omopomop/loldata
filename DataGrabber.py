f85612ca1a49a8bb07351ef430a9677fbe3606df
Eric Wang <wang3021@purdue.edu>
2019-10-17 17:32:23 -0400

    request for division and tier of other summoners

--- DataGrabber.py
+++ DataGrabber.py
@@ -1,14 +1,20 @@
 import requests
 from pip._vendor.distlib.compat import raw_input
-
-def requestSummonerData(region, summonerName, APIKey):
+APIKey = ""
+def requestSummonerData(region, summonerName):
     URL = "https://"+region+"1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key="+APIKey
     response = requests.get(URL)
     return response.json()
-def requestSummonerRanked(region,ID,APIKey):
+def requestSummonerRanked(region,ID):
     URL = "https://"+region+"1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ID+"?api_key="+APIKey
     response = requests.get(URL)
     return response.json()
+def divisionAverages(region,division,tier):
+    URL = "https://"+region+"1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/"+division+"/"+tier+"?page=1"+"?api_key="+APIKey
+    response = requests.get(URL)
+    summJSON = response.json
+    print("size of page 1 is : " + len(summJSON))
+    #for i in range(len(summJSON
 def main():
     region = (str)(raw_input("Enter Region: "))
     summonerName = (str)(raw_input("Enter Summoner Name: "))
@@ -21,5 +27,6 @@ def main():
     print(responseJSON2[1]['tier'])
     print(responseJSON2[1]['rank'])
     print(responseJSON2[1]['leaguePoints'])
+    divisionAverages(region,division,tier):
 if __name__ == "__main__":
     main()
